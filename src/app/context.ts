9a5fc68ee37c36818d3a5eafc46c6f656a9c76c9
Michael Corrigan <mcorrigan89@gmail.com>
2020-09-01 13:20:38 -0700

    remove tweet controller

--- src/app/context.ts
+++ src/app/context.ts
@@ -1,15 +1,12 @@
-import { TweetController } from '@data/tweet/controller';
 import { UserController } from '@data/user/controller';
 import { UserView } from '@data/user/view';
 
 export class Context {
-  public readonly tweetController: TweetController;
   public readonly userController: UserController;
 
   private currentUser: UserView | null = null;
 
   constructor() {
-    this.tweetController = new TweetController(this);
     this.userController = new UserController(this);
   }
 
