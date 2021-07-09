273448d1163e3ec35d2e118050b6b89f2983d4a9
Michael Corrigan <mcorrigan89@gmail.com>
2021-07-08 20:12:49 -0500

    small fixes

--- src/graphql/resolver.types.ts
+++ src/graphql/resolver.types.ts
@@ -1,6 +1,6 @@
 import { GraphQLResolveInfo } from 'graphql';
 import { Context } from '@app/context';
-export type Maybe<T> = T | null;
+export type Maybe<T> = T extends PromiseLike<infer U> ? Promise<U | null> : T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
